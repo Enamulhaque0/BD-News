15225a3f7a2364aeccae9cd15dde24a2b8374f36
Enamulhaque0e <enamulhaq243@gmail.com>
2022-09-03 20:09:50 +0600

    modal added

--- js/app.js
+++ js/app.js
@@ -37,7 +37,7 @@ const data= await res.json()
 
 const allData = data.data
 
-console.log(allData)
+
 
 displayCategory(allData)
 
@@ -55,6 +55,8 @@ const displayCategory = async(allData)=>{
 
   const {_id, total_view, thumbnail_url, details, author, title}= data
 
+  
+
   const {name,img,published_date} = author
 
  const div = document.createElement("div")
@@ -63,26 +65,35 @@ const displayCategory = async(allData)=>{
  div.innerHTML= `
  <div class="card mt-5 mb-4">
             <div class="row">
+
                     <div class="col-md-4">
                         <img src="${thumbnail_url}" class="img-fluid rounded-start h-100" alt="...">
-                    </div>     
+                    </div>   
+
                     <div class="col-md-8">
                         <div class="card-body p-4">
                             <h5 class="card-title">${title.length > 60 ? title.slice(0, 60)+ '...' :title}</h5>
                             <p class="card-text">${details.length > 350 ? details.slice(0, 350)+ '...' : details}</p>
+
                             <div class="d-flex justify-content-between align-items-center mt-3">
                                     <img class="author-img" src="${img}">
                                     <div class="ps-2">
-                                        <p>${name !=="system"? name  : "N/A"}</p>
+                                        <p>${name !=="system" ? name  : "N/A"}</p>
+
                                         <p>${published_date}</p>
                                     </div>
                                 <p>${total_view ? total_view +"M" : "N/A"}</P>
-                                <button type="button" class="btn btn-primary">Primary</button>
+                                
+                                <button onclick="newsDetails('${_id}')" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                               Details
+                                   </button>
+                              
                             </div>
                             
                         <div>
                     </div>
-            </div>
+    </div>
+
  
  
  
@@ -92,5 +103,48 @@ const displayCategory = async(allData)=>{
 
   
 });
+
+}
+
+
+const newsDetails= async (_id)=>{
+  const res= await fetch(`https://openapi.programming-hero.com/api/news/${_id}`)
+
+  const data =  await res.json()
+  
+  const allData = data.data
+
+displayNewsDetails(allData)
+}
+
+
+const displayNewsDetails = async (allData) =>{
+ 
+  
+
+   const data = allData.forEach(data=>{
+
+    const {total_view, author,thumbnail_url,}= data
+    const {name,published_date, img} = author
+    
+    // console.log(total_view)
+
+    const modalBody= document.getElementById('modal-iteam')
+
+    modalBody.innerHTML= `
+    
+    <div class="p-4">
+            <div class="d-flex align-items-center justify-content-between">
+                <p>Author: ${name !=="system" ? name  : "N/A"}</p>
+                <img class="author-image" src="${img}">
+            </div>
+            <p>Total View: ${total_view ? total_view +"M" : "N/A"}</p>
+            <p>Publish Date and Time : ${published_date}</p>
+        </div>
+    
+    `
+   })
+
+  
 
 }
